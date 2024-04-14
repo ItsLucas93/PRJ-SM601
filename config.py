c325e0dba61b52bbd1a98151b7510416a4ab63c6
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-04-14 22:34:14 +0200

    update display configuration

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Projet: PRJ-SM601 - Théorie des Graphes - 2023/2024 - Thème sur l'Ordonnancement
+Auteurs: BAUDET Antoine, SABBEH Chokri, HOUEE Adrien, KOCOGLU Lucas
+Description: Fichier de configuration du projet.
+Version de Python : 3.12
+"""
+
+"""
+Variable de configuration pour la notation des sommets du graphe.
+Si: True -> Notation classique (0, N+1)
+    False -> Notation grecque (α, ω)
+"""
+notation = True
--- fonctions/graph_display.py
+++ fonctions/graph_display.py
@@ -1,5 +1,14 @@
+"""
+Projet: PRJ-SM601 - Théorie des Graphes - 2023/2024 - Thème sur l'Ordonnancement
+Auteurs: BAUDET Antoine, SABBEH Chokri, HOUEE Adrien, KOCOGLU Lucas
+Description: Fichier contenant les fonctions d'affichage du graphe.
+Version de Python : 3.12
+"""
+
 from tabulate import tabulate
 from termcolor import colored
+
+import config
 
 def display_graph_relations(graph_dictionnary):
     """
@@ -47,7 +56,10 @@ def display_graph_relations(graph_dictionnary):
 
     # Affiche les arcs
     for task_id in no_predecessors:
-        print("α" + " -> " + str(task_id) + " = 0")
+        if config.notation:
+            print("0" + " -> " + str(task_id) + " = 0")
+        else:
+            print("α" + " -> " + str(task_id) + " = 0")
 
     sorted_tasks = sorted(graph_dictionnary_flipped.keys())
     for task_id in sorted_tasks:
@@ -55,31 +67,39 @@ def display_graph_relations(graph_dictionnary):
         for successor in successors:
             print(str(task_id) + " -> " + str(successor) + " = " + str(graph_dictionnary[successor][0]))
         if task_id in no_successors:
-            print(str(task_id) + " -> ω = " + str(graph_dictionnary[task_id][0]))
+            if config.notation:
+                print(str(task_id) + " -> " + str(cpt_sommets - 1) + " = " + str(graph_dictionnary[task_id][0]))
+            else:
+                print(str(task_id) + " -> ω = " + str(graph_dictionnary[task_id][0]))
 
 
 def display_graph_matrix(graph_matrix):
     """
     Fonction permettant d'afficher la matrice du graphe.
     """
-    # header = [""] + [colored(str(i), "cyan", attrs=["bold"]) for i in range(len(graph_matrix))]
-    header = [""]
-    for i in range(len(graph_matrix)):
-        if i == 0:
-            header.append(colored("α", "cyan", attrs=["bold"]))
-        elif i == len(graph_matrix) - 1:
-            header.append(colored("ω", "cyan", attrs=["bold"]))
-        else:
-            header.append(colored(str(i), "cyan", attrs=["bold"]))
 
-    # body = [[colored(str(i), "cyan", attrs=["bold"])] + row for i, row in enumerate(graph_matrix)]
-    body = []
-    for i, row in enumerate(graph_matrix):
-        if i == 0:
-            body.append([colored("α", "cyan", attrs=["bold"])] + row)
-        elif i == len(graph_matrix) - 1:
-            body.append([colored("ω", "cyan", attrs=["bold"])] + row)
-        else:
-            body.append([colored(str(i), "cyan", attrs=["bold"])] + row)
+    if config.notation:
+        header = [""] + [colored(str(i), "cyan", attrs=["bold"]) for i in range(len(graph_matrix))]
+    else:
+        header = [""]
+        for i in range(len(graph_matrix)):
+            if i == 0:
+                header.append(colored("α", "cyan", attrs=["bold"]))
+            elif i == len(graph_matrix) - 1:
+                header.append(colored("ω", "cyan", attrs=["bold"]))
+            else:
+                header.append(colored(str(i), "cyan", attrs=["bold"]))
+
+    if config.notation:
+        body = [[colored(str(i), "cyan", attrs=["bold"])] + row for i, row in enumerate(graph_matrix)]
+    else:
+        body = []
+        for i, row in enumerate(graph_matrix):
+            if i == 0:
+                body.append([colored("α", "cyan", attrs=["bold"])] + row)
+            elif i == len(graph_matrix) - 1:
+                body.append([colored("ω", "cyan", attrs=["bold"])] + row)
+            else:
+                body.append([colored(str(i), "cyan", attrs=["bold"])] + row)
 
     print(tabulate(body, headers=header, tablefmt="mixed_outline", numalign="center", stralign="center"))
