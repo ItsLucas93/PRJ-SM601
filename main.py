59b6645820fd719580b8200c69ce2a3dade2d935
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-04-20 16:40:32 +0200

    Comment main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Auteurs: BAUDET Antoine, SABBEH Chokri, HOUEE Adrien, KOCOGLU Lucas
 Description: Ce fichier est le fichier principal du projet, il permet de lancer les exécutions de fonction depuis les autres fichiers python.
 Version de Python : 3.12
 """
-from fonctions.ordonnancement import ordonnencement_graph
 
 """
 Rappel de la structure du Pseudo-code :
@@ -29,7 +28,6 @@ Début
     fin Tant que
 FIn
 """
-
 from termcolor import colored
 
 from filemanager.filemanager import files_list
@@ -37,6 +35,7 @@ from filemanager.filemanager import read_file
 from fonctions.graph_display import display_graph_matrix
 from fonctions.graph_display import display_graph_relations
 from fonctions.validators import ordonnancement_validator
+from fonctions.ordonnancement import ordonnencement_graph
 
 
 # Fonctions
@@ -56,16 +55,22 @@ def welcome():
 
 def menu_principal(choix=0):
     """
-    Fonction permettant d'afficher le menu principal.
-    Tant que l’utilisateur décide de tester un tableau de contraintes faire
-    Choisir le tableau de contraintes à traiter
+     * Fonction: menu_principal
+     * -----------------
+     * Fonction permettant d'afficher le menu principal.
+     * Tant que l’utilisateur décide de tester un tableau de contraintes faire
+     * Choix possibles :
+     * 1. Lire un tableau de contraintes sur fichier
+     * 2. Quitter le programme
     """
+    # Boucle principale
     while True:
         print("---------------------- Menu Principal ----------------------"
               "\n1.\tLire un tableau de contraintes sur fichier"
               "\n2.\tQuitter le programme"
               "\n----------------------------------------------------------")
         try:
+            # Demande de choix
             choix = int(input(colored("Entrez votre choix : ", "magenta")))
             match choix:
                 case 1:
@@ -82,9 +87,18 @@ def menu_principal(choix=0):
 
 def menu_graphe(choix=0):
     """
-    Fonction permettant d'afficher le menu du graphe.
+     * Fonction: menu_graphe
+     * -----------------
+     * Fonction permettant d'afficher le menu de traitement des graphes.
+     * Un menu s'affiche permettant de choisir parmi une liste de fichiers de test dans le dossier testfiles.
+     * L'utilisateur peut choisir un fichier de test pour le traiter, ou sortir du menu avec le choix 0.
+     * Après le choix d'un fichier, le programme appelle les fonctions pour lire le graphes sous différents affichages.
+     * Et vérifie si le graphe est un graphe d'ordonnancement.
+     * Si c'est le cas, le programme propose de continuer le traitement du graphe d'ordonnancement.
+     * Sinon, le programme propose de retourner au menu de choix de tableau.
     """
-
+    # Récupération dans la première variable le text d'affichage, dans la seconde variable un dictionnaire avec comme
+    # clé l'index et comme valeur le chemin du fichier
     string_test_files, index_test_files = files_list()
 
     if index_test_files is not None:
@@ -93,32 +107,40 @@ def menu_graphe(choix=0):
               "----------------------------------------------------------")
         while True:
             try:
+                # Demande de choix
                 choix = int(input(colored("Entrez le numéro du fichier à traiter : ", "magenta")))
+
+                # Sortie du menu
                 if index_test_files[choix] == "Retour au menu principal":
                     break
+                # Traitement du fichier
                 elif choix in index_test_files.keys():
-                    # print(read_file(index_test_files[choix])[0])
+                    # Lecture du fichier, avec en première variable un dictionnaire contenant le graph, et en seconde
+                    # variable une matrice de valeurs
                     graph_dict, graph_matrix = read_file(index_test_files[choix])
+
+                    # Affichage des relations du graphe
                     print("----------------------------------------------------------")
                     display_graph_relations(graph_dict)
                     input(colored("Appuyez sur une touche pour continuer...", "magenta"))
+
+                    # Affichage de la matrice de valeurs
                     print("----------------------------------------------------------")
                     print("* " + colored("Matrice de valeurs :", attrs=["bold", "underline"]))
                     display_graph_matrix(graph_matrix)
                     input(colored("Appuyez sur une touche pour continuer...", "magenta"))
-                    print("----------------------------------------------------------")
+
                     # Vérification des propriétés du graphe d'ordonnancement
                     validator, adjacency_matrix = ordonnancement_validator(graph_matrix, graph_dict)
+                    print("----------------------------------------------------------")
+                    # Si validation OK, c'est un graphe d'ordonnancement
                     if validator:
+                        # Demande de confirmation pour continuer le graph d'ordonnancement
                         confirm = None
                         while confirm not in ['y', 'n']:
                             confirm = input(colored("Souhaitez-vous continuer ? (y/n)... ", "magenta"))
                             match confirm:
                                 case 'y':
-                                    # TODO: Calculer les rangs des sommets et les afficher
-                                    # TODO: Calculer les calendriers au plus tôt et au plus tard et les afficher
-                                    # TODO: Calculer les marges et les afficher
-                                    # TODO: Calculer le(s) chemin(s) critique(s) et les afficher
                                     ordonnencement_graph(adjacency_matrix, graph_matrix)
                                     break
                                 case 'n':
@@ -130,11 +152,14 @@ def menu_graphe(choix=0):
                                     break
                                 case _:
                                     print(colored("Le choix n'a pas été reconnue.", "red"))
+
+                    # Si validation pas OK, retour au menu de choix de tableau
                     else:
                         input(colored("Appuyez sur une touche pour continuer (retour au choix de tableau)...", "magenta"))
                         print("---------------------- Menu Graphe ----------------------"
                               + string_test_files +
                               "----------------------------------------------------------")
+                # Choix non reconnu
                 else:
                     print(colored("Le choix n'a pas été reconnue.", "red"))
             except ValueError:
