8e70b552baa3e0c07c9013f13b86f7820c82a8bf
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-04-21 00:18:49 +0200

    Update D_3_graph_display.py
    comment code

--- fonctions/D_3_graph_display.py
+++ fonctions/D_3_graph_display.py
@@ -10,22 +10,28 @@ from termcolor import colored
 
 import D_3_config as config
 
+
 def display_graph_relations(graph_dictionnary):
     """
-    Affichage du graphe comme un jeu de triplets, par exemple:
-
-    * Création du graphe d’ordonnancement :
-    7 sommets
-    9 arcs
-    0 -> 1 = 0
-    0 -> 2 = 0
-    1 -> 3 = 1
-    1 -> 4 = 1
-    2 -> 4 = 2
-    2 -> 5 = 2
-    3 -> 6 = 3
-    4 -> 5 = 4
-    5 -> 6 = 5
+     * Fonction: display_graph_relations
+     * ---------------------------------
+     * Fonction permettant d'afficher les relations du graphe.
+     * :param graph_dictionnary: Graphe sous forme de dictionnaire.
+     *
+     * Affichage du graphe comme un jeu de triplets, par exemple:
+     *
+     * * Création du graphe d’ordonnancement :
+     * 7 sommets
+     * 9 arcs
+     * 0 -> 1 = 0
+     * 0 -> 2 = 0
+     * 1 -> 3 = 1
+     * 1 -> 4 = 1
+     * 2 -> 4 = 2
+     * 2 -> 5 = 2
+     * 3 -> 6 = 3
+     * 4 -> 5 = 4
+     * 5 -> 6 = 5
     """
     no_successors = set(graph_dictionnary.keys())
     no_predecessors = set()
@@ -46,26 +52,31 @@ def display_graph_relations(graph_dictionnary):
 
     print("* " + colored("Affichage du graphe :", attrs=["bold", "underline"]) +
           "\n" + str(cpt_sommets) + " sommets"
-          "\n" + str(cpt_arcs) + " arcs")
+                                    "\n" + str(cpt_arcs) + " arcs")
 
     # Création d'un dictionnaire inversé pour faire apparaître les arcs triés par le numéro de tâche
-    graph_dictionnary_flipped = {task_id: (duration, []) for task_id, (duration, predecessors) in graph_dictionnary.items()}
+    graph_dictionnary_flipped = {task_id: (duration, []) for task_id, (duration, predecessors) in
+                                 graph_dictionnary.items()}
     for task_id, (duration, predecessors) in graph_dictionnary.items():
         for predecessor in predecessors:
             graph_dictionnary_flipped[predecessor][1].append(task_id)
 
-    # Affiche les arcs
+    # Affichage de l'arc en provenance du sommet fictif α vers les sommets sans prédécesseurs
     for task_id in no_predecessors:
         if config.notation:
             print("0" + " → " + str(task_id) + " = 0")
         else:
             print("α" + " → " + str(task_id) + " = 0")
 
+    # Tri des tâches pour affichage
     sorted_tasks = sorted(graph_dictionnary_flipped.keys())
+
+    # Affichage des arcs
     for task_id in sorted_tasks:
         duration, successors = graph_dictionnary_flipped[task_id]
         for successor in successors:
             print(str(task_id) + " → " + str(successor) + " = " + str(graph_dictionnary[task_id][0]))
+        # Affichage des arcs en provenance des sommets sans successeurs vers le sommet fictif ω
         if task_id in no_successors:
             if config.notation:
                 print(str(task_id) + " → " + str(cpt_sommets - 1) + " = " + str(graph_dictionnary[task_id][0]))
@@ -75,9 +86,17 @@ def display_graph_relations(graph_dictionnary):
 
 def display_graph_matrix(graph_matrix, option=""):
     """
-    Fonction permettant d'afficher la matrice du graphe.
+     * Fonction: display_graph_matrix
+     * -----------------------------
+     * Fonction permettant d'afficher la matrice quelconque du graphe.
+     * Il existe des options d'affichage pour mettre en évidence des valeurs.
+     * "diag" : Met en évidence les valeurs diagonales, avec en rouge les valeurs non-nulles et en vert les valeurs nulles.
+     * "adjacency" : Met en évidence les valeurs non nulles et en gris les valeurs nulles.
+     * :param graph_matrix: Matrice de valeurs du graphe.
+     * :param option: Option d'affichage de la matrice.
     """
 
+    # Création du header de la matrice
     if config.notation:
         header = [""] + [colored(str(i), "cyan", attrs=["bold"]) for i in range(len(graph_matrix))]
     else:
@@ -90,6 +109,7 @@ def display_graph_matrix(graph_matrix, option=""):
             else:
                 header.append(colored(str(i), "cyan", attrs=["bold"]))
 
+    # Création du corps de la matrice
     if config.notation:
         body = [[colored(str(i), "cyan", attrs=["bold"])] + row for i, row in enumerate(graph_matrix)]
     else:
@@ -102,10 +122,14 @@ def display_graph_matrix(graph_matrix, option=""):
             else:
                 body.append([colored(str(i), "cyan", attrs=["bold"])] + row)
 
+    # Mise en forme des valeurs de la matrice : diagonale
     if "diag" in option:
         for i in range(len(body)):
             for j in range(1, len(body[i])):
                 if i == j - 1:
+                    # Mise en évidence des valeurs diagonales
+                    # Si la valeur est nulle, elle respecte la propriété pour un graphe d'ordonnancement, donc en vert
+                    # Sinon, elle ne respecte pas la propriété, donc en rouge
                     if body[i][j] == 0:
                         body[i][j] = colored(body[i][j], "light_green", attrs=["bold"])
                     else:
@@ -116,9 +140,11 @@ def display_graph_matrix(graph_matrix, option=""):
                     else:
                         body[i][j] = colored(body[i][j], attrs=["bold"])
 
+    # Mise en forme des valeurs de la matrice : adjacence
     if "adjacency" in option:
         for i in range(len(body)):
             for j in range(1, len(body[i])):
+                # Mise en évidence des valeurs non nulles
                 if body[i][j] == 0:
                     body[i][j] = colored(body[i][j], "dark_grey", attrs=[])
                 else:
