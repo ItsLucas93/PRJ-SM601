22b33b72bd689710e1bd822743ba489e335578a5
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-04-25 22:58:28 +0200

    Update D_3_validators.py

--- fonctions/D_3_validators.py
+++ fonctions/D_3_validators.py
@@ -1,6 +1,6 @@
 """
 Projet: PRJ-SM601 - Théorie des Graphes - 2023/2024 - Thème sur l'Ordonnancement
-Auteurs: BAUDET Antoine, SABBEH Chokri, HOUEE Adrien, KOCOGLU Lucas
+Auteur: KOCOGLU Lucas
 Description: Ce fichier est le fichier qui contient les fonctions de validation du graphe d'ordonnancement.
 Version de Python : 3.12
 """
